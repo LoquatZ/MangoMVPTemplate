84072f8e39e12b789ba61a4edcda02bfbec12852
GitHub Action <action@github.com>
2021-04-27 14:03:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/loquatz/mangomvptemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/loquatz/mangomvptemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.loquatz.mangomvptemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.loquatz.mangomvptemplate.MyBundle
 
 class MyApplicationService {
 
